03c27b535f50f271583291cbcaf66b218b9409c4
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-13 20:13:25 -0300

    incluir todos exercicios

---
+++ exercises/4_1/exercicio 2.js
@@ -0,0 +1,16 @@
+
+// Faça um programa que retorne o maior de dois números. 
+// Defina, no começo do programa, duas variáveis com os valores que serão comparados.
+
+var a = 10;
+var b = 20;
+
+if (a > b) {
+    console.log("a é maior que b");
+}
+  } 
+    else {
+  console.log("b é maior que a");
+    }
+};
+
