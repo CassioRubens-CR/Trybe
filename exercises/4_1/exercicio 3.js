52688b9c9326c58ced59aedbd50f650be26c6431
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-13 20:46:42 -0300

    exercicio 3

---
+++ exercises/4_1/exercicio 3.js
@@ -0,0 +1,14 @@
+// Faça um programa que retorne o maior de três números. 
+// Defina, no começo do programa, três variáveis com os valores que serão comparados.
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+if (a > b && a > c) {
+  console.log("O maior número é: " + a + " (a)");
+} else if (b > a && b > c) {
+  console.log("O maior número é: " + b + " (b)");
+} else {
+  console.log("O maior número é: " + c + " (c)");
+}
