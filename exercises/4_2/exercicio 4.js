db8baa2d985d49f17597db79c124fd7b107cedf8
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-14 19:26:36 -0300

    exercico 4

---
+++ exercises/4_2/exercicio 4.js
@@ -0,0 +1,25 @@
+// Você irá utilizar esse array nos exercícios:
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Com o mesmo código do exercício anterior,
+// caso valor final seja maior que 20, imprima a mensagem: 
+// "valor maior que 20". Caso não seja, imprima a mensagem:
+// "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (let number = 0; number < numbers.length; number += 1) {
+    soma = soma + numbers[number];
+}
+
+let media = soma / numbers.length;
+
+console.log(media);
+
+if (media > 20) {
+    console.log("valor maior que 20")
+} else {
+    console.log("valor menor ou igual a 20")
+}
