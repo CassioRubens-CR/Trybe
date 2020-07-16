6007438b971d3dc3335169d3c1c65fb68bb7349f
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-16 19:03:59 -0300

    exercicio 5 parte 1

---
+++ exercises/4_4/exercicio 5 - Parte 1.js
@@ -0,0 +1,24 @@
+// Agora, defina um segundo objeto com a mesma estrutura 
+// (as mesmas chaves) do primeiro e os seguintes valores:
+// "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+// "O último MacPatinhas", "Sim".
+
+
+let info1 = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "sim",
+  };
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Chistmas on Bear Mountain",
+    nota: "Dell's Four Color Comics #178",
+    MacPatinhas: "Sim",
+  };
+
+console.log(info1.personagem + " e " + info2.personagem);
+console.log(info1.origem + " e " + info2.origem);
+console.log(info1.nota + " e " + info2.nota);
+console.log(info1.recorrente + " e " + info2.MacPatinhas);
