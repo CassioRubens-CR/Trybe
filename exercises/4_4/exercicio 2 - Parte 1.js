c873574b31aabee7530fa610600acd2da93ccd12
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-16 18:30:02 -0300

    exercicio 2 parte 1

---
+++ exercises/4_4/exercicio 2 - Parte 1.js
@@ -0,0 +1,15 @@
+// Insira no objeto uma nova propriedade 
+// com o nome de chave "recorrente" e o valor "Sim" e,
+// em seguida, imprima o objeto no console.
+
+let saudacao = "Bem vinda, "
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "sim",
+  };
+  console.log(saudacao + info["personagem"]);
+  console.log(info["recorrente"]);
+    
