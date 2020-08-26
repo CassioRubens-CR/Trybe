d847ed1f11178dc13ff14dcda7b6ea614dca1eb8
Cássio Rubens <cassiorubensadv@gmail.com>
2020-08-26 18:49:55 -0300

    Exercises 1

---
+++ exercises/9_1/exercises1.js
@@ -0,0 +1,36 @@
+// 1. Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+console.log(planetDistanceFromSun(venus)); // B
+console.log(planetDistanceFromSun(jupiter)); // C
+
+// Resposta A, B, C: 
+// A) Mars is 227900000 kilometers apart from the Sun
+// B) Venus is 108200000 kilometers apart from the Sun
+// C) Jupiter is 778500000 kilometers apart from the Sun
