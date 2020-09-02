1d6a52b6e1717224f2e9d93005c52b862f18fbb3
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-02 19:29:53 -0300

    Exercises 1

---
+++ exercises/10_1/exercises1.test.js
@@ -0,0 +1,15 @@
+// Exercico 1
+// A função sum(a, b) retorna a soma do parâmetro a com o b
+// Teste se o retorno de sum(4, 5) é 9
+// Teste se o retorno de sum(0, 0) é 0
+// Teste se a função sum lança um erro quando os parametros são 4 e "5"(string 5)
+// Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+const sum = require('./exercises1');
+
+test('exercicio1', () => {
+  expect(typeof sum).toBe('function');
+})
+// implemente seus testes aqui
+// assert.strictEqual(sum(4, 5), 9);
+// assert.strictEqual(sum(0, 0), 0);
+// assert.strictEqual(sum(4, "5"), 9); // apresenta erro "parameters must be numbers"
