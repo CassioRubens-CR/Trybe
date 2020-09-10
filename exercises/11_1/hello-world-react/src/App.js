34eb588bd151047b841bd4e5fdfe926a52e590c6
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-10 18:48:26 -0300

    exercises 2

--- exercises/11_1/hello-world-react/src/App.js
+++ exercises/11_1/hello-world-react/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const commitments = ['Tomas café', 'Tomar banho', 'Trabalhar', 'Almoçar', 'Estudar na Trybe!', 'Jantar', 'Dormir'];
+
+const task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>{value}</li>
   );
 }
 
+function App() {
+  return (
+    <ul>
+      <div><h1>Compromissos</h1></div>
+      {commitments.map(mayCommitments => task(mayCommitments))}
+    </ul>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 export default App;
