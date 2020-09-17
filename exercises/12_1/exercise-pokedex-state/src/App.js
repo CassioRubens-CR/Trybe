4edd593396669d0e0d7d7f037572f5f00130a9a1
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-17 17:15:58 -0300

    Button Fire

--- exercises/12_1/exercise-pokedex-state/src/App.js
+++ exercises/12_1/exercise-pokedex-state/src/App.js
@@ -9,9 +9,11 @@ class App extends React.Component {
     super()
 
     this.handleClick = this.handleClick.bind(this)
+    this.handleClickFire = this.handleClickFire.bind(this)
     
     this.state = {
-      index: 0
+      index: 0,
+      // type: ''
     }
   }
   
@@ -23,12 +25,33 @@ class App extends React.Component {
     this.setState({ index: newIndex });
   }
 
+  handleClickFire() {
+    let newIndex = pokemons.findIndex((poke, i) => poke.type === 'Fire' && i > this.state.index);
+    if(newIndex < 0) {
+      newIndex = pokemons.findIndex(poke => poke.type === 'Fire')
+    }
+    this.setState({ index: newIndex });
+    // let typePokemon = this.state.type;
+    // this.setState({typePokemon});
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Pokedex </h1>
-        <button onClick={this.handleClick}>Próximo pokémon</button>
-        <Pokemon pokemon={pokemons[this.state.index]} />
+        <div>
+          <button onClick={this.handleClick}>Próximo pokémon</button>
+          <Pokemon pokemon={pokemons[this.state.index]} />
+          {/* <div className='buttonFire'>
+          {pokemons.filter(poke => poke.type === this.state.type)
+          .map(pokeFire => pokeFire.type === 'Fire')
+          } */}
+          <button onClick={this.handleClickFire}>Próximo pokémon</button>
+
+          <button onClick={this.handleClick}>Próximo pokémon</button>
+        
+          <button onClick={this.handleClick}>Próximo pokémon</button>
+        </div>
         {/* <Pokedex pokemons={pokemons} /> */}
       </div>
     );
